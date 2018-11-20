b694d1ca5ef54538c4227ff0723ac2d548036c96
Mahmoud Agamy <agamyv@gmail.com>
2018-11-20 22:49:47 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -14,6 +14,13 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
   
+<<<<<<< HEAD
 
+=======
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre 
+  end
+>>>>>>> 8c87ef559f2236a5392c6fe0bd1793d2944d862b
 
 end
