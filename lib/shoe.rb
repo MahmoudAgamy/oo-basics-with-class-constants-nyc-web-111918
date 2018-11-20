c44a00b161f1a1859eb6f89a138babc0a0cc4697
Mahmoud Agamy <agamyv@gmail.com>
2018-11-20 22:55:29 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,5 @@
 class Shoe
+  
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   
@@ -13,14 +14,5 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-  
-<<<<<<< HEAD
-
-=======
-  def genre=(genre)
-    @genre = genre
-    GENRES << genre 
-  end
->>>>>>> 8c87ef559f2236a5392c6fe0bd1793d2944d862b
 
 end
